7633cdf42c29501d44ade849cc0fac9b9dc30e1e
Weikang Li <liwk@smail.nju.edu.cn>
2020-05-26 23:14:15 +0800

    update HW5

--- HW5/ps5code/main.py
+++ HW5/ps5code/main.py
@@ -7,25 +7,28 @@ class KNN():
     #k: int,最近邻个数.
     def __init__(self, k=5):
         self.k = k
-
-    # 此处需要填写，建议欧式距离，计算一个样本与训练集中所有样本的距离
+        
     def distance(self, one_sample, X_train):
-       
-        return 
+        dist = [np.linalg.norm(one_sample - x) for x in X_train]
+        return dist
     
     # 此处需要填写，获取k个近邻的类别标签
     def get_k_neighbor_labels(self, distances, y_train, k):
-        
-        return 
+        idx = np.argsort(distances)[:k]
+        k_nearest_neighbors = np.array([y_train[i] for i in idx])
+        return k_nearest_neighbors
+    
     # 此处需要填写，标签统计，票数最多的标签即该测试样本的预测标签
     def vote(self, one_sample, X_train, y_train, k):
-        
-        return 
+        dist = self.distance(one_sample, X_train)
+        neighbor_labels = self.get_k_neighbor_labels(dist, y_train, k)
+        counts = np.bincount(neighbor_labels.astype('int'))
+        return counts.argmax()
     
     # 此处需要填写，对测试集进行预测
     def predict(self, X_test, X_train, y_train):
-        
-        return 
+        y_preds = np.apply_along_axis(self.vote, 1, X_test, X_train, y_train, self.k)
+        return y_preds
   
 
 def main():
@@ -36,8 +39,7 @@ def main():
    
     #将预测值存入y_pred(list)内    
     y_pred = clf.predict(test_data, train_data, train_labels)
-    np.savetxt("test_ypred.csv", y_pred, delimiter=' ')
-  
+    np.savetxt("test_ypred.csv", y_pred, fmt="%d", delimiter=' ')
 
 
 if __name__ == "__main__":
